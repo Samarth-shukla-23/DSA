4982ccb5f527dad3c9acb77c5a9b23923da7d155
Samarth-shukla-23 <samarthshukla876@gmail.com>
2026-09-19 23:44:40 +0530

    Time: 14 ms (39.75%) | Memory: 12.4 MB (56.74%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0 or(x%10==0 and x!=0):
+            return False
+        revert_no = 0
+        while x > revert_no :
+            revert_no = revert_no*10+x%10
+            x//=10
+        return x==revert_no or x==revert_no//10
+        
+
+        
